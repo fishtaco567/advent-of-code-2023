6204da9f7a4940ec47dc08421f9554271132ac8b
fishtaco567 <tgamer27@gmail.com>
2023-12-04 20:54:09 -0800

    day34

--- src/solutions/day_3.rs
+++ src/solutions/day_3.rs
@@ -1,1 +1,120 @@
-pub fn solve(_input: &str) {}
+use std::collections::{HashMap, HashSet};
+
+pub fn solve(input: &str) {
+    let mut sum = 0;
+
+    let grid = CharGrid::new(input);
+
+    let lines_len = grid.source.len();
+
+    let mut gears = HashMap::new();
+
+    for (i, line) in input.lines().enumerate() {
+        let mut state: Option<(usize, Vec<char>)> = None;
+
+        for (ic, char) in line.char_indices() {
+            if char.is_numeric() {
+                if let Some((_, x)) = &mut state {
+                    x.push(char);
+                } else {
+                    state = Some((ic, vec![char]));
+                }
+            } else {
+                if let Some((j, xx)) = &state {
+                    let num = xx.iter().collect::<String>().parse::<i32>().unwrap();
+
+                    let mut near = false;
+                    let mut near_star = HashSet::new();
+                    for q in *j..(*j + xx.len()) {
+                        for x in q.saturating_sub(1)..=(q + 1).min(grid.width - 1) {
+                            for y in i.saturating_sub(1)..=(i + 1).min(lines_len - 1) {
+                                let c = grid.at(x, y);
+                                if !c.is_numeric() && c != '.' {
+                                    near |= true;
+                                }
+
+                                if c == '*' {
+                                    near_star.insert((x, y));
+                                }
+                            }
+                        }
+                    }
+
+                    if near {
+                        sum += num;
+                    }
+
+                    for (x, y) in near_star {
+                        gears
+                            .entry((x, y))
+                            .and_modify(|x: &mut Vec<i32>| x.push(num))
+                            .or_insert(vec![num]);
+                    }
+                }
+
+                state = None;
+            }
+        }
+
+        if let Some((j, xx)) = &state {
+            let num = xx.iter().collect::<String>().parse::<i32>().unwrap();
+
+            let mut near = false;
+            let mut near_star = HashSet::new();
+            for q in *j..(*j + xx.len()) {
+                for x in q.saturating_sub(1)..=(q + 1).min(grid.width - 1) {
+                    for y in i.saturating_sub(1)..=(i + 1).min(lines_len - 1) {
+                        let c = grid.at(x, y);
+                        if !c.is_numeric() && c != '.' {
+                            near |= true;
+                        }
+
+                        if c == '*' {
+                            near_star.insert((x, y));
+                        }
+                    }
+                }
+            }
+
+            if near {
+                sum += num;
+            }
+
+            for (x, y) in near_star {
+                gears
+                    .entry((x, y))
+                    .and_modify(|x: &mut Vec<i32>| x.push(num))
+                    .or_insert(vec![num]);
+            }
+        }
+    }
+
+    println!("{sum}");
+
+    let sum_pt2: i64 = gears
+        .values()
+        .filter(|x| x.len() == 2)
+        .map(|x| x[0] as i64 * x[1] as i64)
+        .sum();
+    println!("{sum_pt2}");
+}
+
+struct CharGrid<'a> {
+    source: Vec<&'a str>,
+    width: usize,
+}
+
+impl<'a> CharGrid<'a> {
+    pub fn new(source: &'a str) -> Self {
+        let lines: Vec<&'a str> = source.lines().collect();
+        let wid = lines[0].len();
+        Self {
+            source: lines,
+            width: wid,
+        }
+    }
+
+    pub fn at(&self, x: usize, y: usize) -> char {
+        self.source[y].chars().nth(x).unwrap()
+    }
+}
--- src/solutions/day_4.rs
+++ src/solutions/day_4.rs
@@ -1,1 +1,57 @@
-pub fn solve(_input: &str) {}
+use crate::iter_helper::IterHelper;
+use itertools::Itertools;
+use std::collections::HashSet;
+
+pub fn solve(input: &str) {
+    let mut res: i32 = 0;
+
+    let mut scores = Vec::new();
+
+    for line in input.lines() {
+        let (_, rest) = line.split_once(": ").unwrap();
+        let (winning, mine) = rest.split_once(" | ").unwrap();
+
+        let winning: HashSet<i32> = winning.split_ascii_whitespace().parse_all().collect();
+        let mine = mine
+            .split_ascii_whitespace()
+            .parse_all::<i32>()
+            .collect_vec();
+
+        let mut score = None;
+        let mut won = 0;
+
+        for num in mine {
+            if winning.contains(&num) {
+                if let Some(s) = score {
+                    score = Some(s * 2);
+                } else {
+                    score = Some(1);
+                }
+
+                won += 1;
+            }
+        }
+
+        scores.push(won);
+
+        if let Some(score) = score {
+            res += score;
+        }
+    }
+
+    let mut num_cards = vec![1; scores.len()];
+
+    for i in 0..scores.len() {
+        let score = scores[i];
+        let num_of = num_cards[i];
+
+        for j in 1..=score {
+            num_cards[i + j] += num_of;
+        }
+    }
+
+    let part_2 = num_cards.iter().sum::<i64>();
+
+    println!("Part 1 is: {res}");
+    println!("Part 2 is: {part_2}");
+}
